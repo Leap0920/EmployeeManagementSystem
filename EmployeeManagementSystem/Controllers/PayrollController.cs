477a16ed3a92eaacf11c7f1f4f834e585f67af55
Leap0920 <carlobaclao123@gmail.com>
2025-09-14 21:27:59 +0800

    Add project files.

---
+++ EmployeeManagementSystem/Controllers/PayrollController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class PayrollController : Controller
+    {
+        [HttpGet]
+        public ActionResult Salary()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Salary(string employeeID, string employeeName, string position, string gender, decimal ratePerDay, int daysWorked)
+        {
+            decimal grossPay = ratePerDay * daysWorked;
+            decimal sss = grossPay * 0.05m;
+            decimal philHealth = grossPay * 0.05m;
+            decimal pagIbig = grossPay * 0.05m;
+            decimal tax = grossPay * 0.05m;
+            decimal netPay = grossPay - (sss + philHealth + pagIbig + tax);
+
+            ViewBag.EmployeeID = employeeID;
+            ViewBag.EmployeeName = employeeName;
+            ViewBag.Position = position;
+            ViewBag.Gender = gender;
+            ViewBag.RatePerDay = ratePerDay;
+            ViewBag.DaysWorked = daysWorked;
+            ViewBag.GrossPay = grossPay;
+            ViewBag.SSS = sss;
+            ViewBag.PhilHealth = philHealth;
+            ViewBag.PagIbig = pagIbig;
+            ViewBag.Tax = tax;
+            ViewBag.NetPay = netPay;
+
+            return View("SalaryResult");
+        }
+    }
+}
---
+++ EmployeeManagementSystem/Controllers/VotingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class VotingController : Controller
+    {
+        [HttpGet]
+        public ActionResult Age()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Age(int age, bool isRegistered = false)
+        {
+            string result;
+
+            if (age >= 18 && isRegistered)
+                result = "Eligible to Vote";
+            else if (age >= 18 && !isRegistered)
+                result = "Please register first";
+            else if (age < 18 && isRegistered)
+                result = "Invalid Input";
+            else
+                result = "Invalid Input";
+
+            ViewBag.Age = age;
+            ViewBag.IsRegistered = isRegistered;
+            ViewBag.Result = result;
+
+            return View("AgeResult");
+        }
+    }
+}
